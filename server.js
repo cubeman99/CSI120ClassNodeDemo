47771cebbd6616f75c39fe306adeb769c84aa788
cubeman99 <jordand95@gmail.com>
2013-11-12 18:48:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send({
+  res.json({
     "name" : "David Jordan",
     "vacation" : {
       "places": ["45.64, -73.63","40.760, -73.973","32.750, -117.07"]
